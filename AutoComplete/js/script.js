4a73fb738c52c64c414c1c78497f1fbfcbcba3bc
Amin Alaee <112581116+aiaaee@users.noreply.github.com>
2024-10-18 19:48:57 +0330

    Update script.js

--- AutoComplete/js/script.js
+++ AutoComplete/js/script.js
@@ -1,1 +1,53 @@
 
+let $ = document ;
+let autoCompleteWrapper = $.querySelector('.search-input') ; 
+let searchInputElem = $.querySelector('input') ;
+let autoCompleteBox = $.querySelector(".autocom-box") ;
+
+searchInputElem.addEventListener('keyup' , function(){
+    let searchValue = searchInputElem.value ; 
+    if(searchValue){
+        autoCompleteWrapper.classList.add("active")
+        let filteredWords = suggestions.filter(function(word){
+            return word.toLowerCase().includes(searchValue.toLowerCase()) 
+        })
+        suggestionWordsGenerator(filteredWords)
+    }
+    else{
+        autoCompleteWrapper.classList.remove('active')
+    }
+})
+
+
+function suggestionWordsGenerator(wordsArray){
+    let listItemArray = wordsArray.map(function(word){
+        console.log(word)
+        return `<li>${word}</li>`
+    })
+
+
+    let CustomListItem 
+    if (!listItemArray.length) {
+        CustomListItem = `<li>${searchInputElem.value}</li>` 
+    }
+    else{
+        CustomListItem = listItemArray.join('')        
+    }
+
+    autoCompleteBox.innerHTML = CustomListItem
+
+    select()
+
+}
+
+
+
+function select(){
+    let allListItems = autoCompleteBox.querySelectorAll('li')
+    allListItems.forEach(function(wordItem){
+        wordItem.addEventListener('click' , function(event){
+            searchInputElem.value = event.target.textContent
+            autoCompleteWrapper.classList.remove('active')
+        })
+    })
+}
